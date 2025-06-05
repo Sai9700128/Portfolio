bb8b869549e4c160a3fd89e2c971ce48dd97e27d
Sai9700128 <burra.sa@northeastern.edu>
2025-06-05 21:22:20 +0530

    divided all the code into components

---
+++ Client/src/components/skills.jsx
@@ -0,0 +1,34 @@
+export const skills = [
+  {
+    id: 1,
+    category: "Cloud Platforms",
+    name: "AWS",
+    percentage: 90,
+    visualBar: "█████████░",
+    details: "EC2, S3, IAM, Lambda, RDS, VPC, Route 53, ECS, EKS"
+  },
+  {
+    id: 2,
+    category: "Infrastructure as Code",
+    name: "Terraform",
+    percentage: 90,
+    visualBar: "█████████░",
+    details: "Remote state, Reusable modules, Workspaces"
+  },
+  {
+    id: 3,
+    category: "Containerization & Orchestration",
+    name: "Docker/Kubernetes",
+    percentage: 80,
+    visualBar: "████████░░",
+    details: "Docker, Kubernetes, Helm"
+  },
+  {
+    id: 4,
+    category: "CI/CD & Automation",
+    name: "GitHub Actions/Jenkins",
+    percentage: 90,
+    visualBar: "█████████░",
+    details: "GitHub Actions, Jenkins, Bash, ArgoCD"
+  }
+];
